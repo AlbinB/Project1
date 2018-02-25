951c17d3d389206f561fd2101f800bc6430f9f1a
AlbinB <albinb@live.com>
2018-02-25 14:54:12 -0500

    adding dropout with 50% keep probability

--- censusData.py
+++ censusData.py
@@ -15,16 +15,13 @@ import dataUtils
 training_data, training_labels = dataUtils.readData("project1trainingdata.csv")
 test_data, test_labels = dataUtils.readData("project1testdata.csv")
 
-
-# Build tensorflow blueprint
-
-## Tensorflow placeholder
+# Tensorflow placeholder
 input_placeholder = tf.placeholder(tf.float32, shape=[None, 113])
 
 ## Neural network hidden layers
 
-###Long Way
 """
+#Long Way
 weight1 = tf.get_variable("weight1", shape=[113, 226], initializer=tf.contrib.layers.xavier_initializer())
 bias1 = tf.get_variable("bias1", shape=[226], initializer=tf.contrib.layers.xavier_initializer())
 hidden_layer1 = tf.nn.relu(tf.matmul(input_placeholder, weight1)+bias1)
@@ -38,39 +35,36 @@ bias3 = tf.get_variable("bias3", shape=[100], initializer=tf.contrib.layers.xavi
 hidden_layer3 = tf.nn.relu(tf.matmul(hidden_layer2, weight3)+bias3)
 ###
 """
-
 ###Short Way
-hidden_layer1 = tf.layers.dense(input_placeholder, 226, activation=tf.nn.relu)
-hidden_layer2 = tf.layers.dense(hidden_layer1, 150, activation=tf.nn.relu)
-hidden_layer3 = tf.layers.dense(hidden_layer2, 100, activation=tf.nn.relu)
+hidden_layer1 = tf.nn.dropout(tf.layers.dense(input_placeholder, 226, activation=tf.nn.relu),keep_prob=0.5)
+hidden_layer2 = tf.nn.dropout(tf.layers.dense(hidden_layer1, 226, activation=tf.nn.relu),keep_prob=0.5)
+hidden_layer3 = tf.nn.dropout(tf.layers.dense(hidden_layer2, 113, activation=tf.nn.relu),keep_prob=0.5)
 
-## Logit/Output layer
+# Logit/Output layer
 logits = tf.nn.softmax(tf.layers.dense(hidden_layer3, 2, activation=None))
 
 
-## label placeholder
+# label placeholder
 label_placeholder = tf.placeholder(tf.float32, shape=[None, 2])
 
-## loss function
+# loss function
 loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=label_placeholder, logits=logits))
 
-
-## backpropagation algorithm
+# backpropagation algorithm
 train = tf.train.AdamOptimizer().minimize(loss)
 
 accuracy = dataUtils.accuracy(logits, label_placeholder)
 
-## Make tensorflow session
+# Make tensorflow session
 with tf.Session() as sess:
     ## Initialize variables
     sess.run(tf.global_variables_initializer())
-
 
     step_count = 0
     while True:
         step_count += 1
 
-        batch_training_data, batch_training_labels = dataUtils.getBatch(data=training_data, labels=training_labels, batch_size=100)
+        batch_training_data, batch_training_labels = dataUtils.getBatch(data=training_data, labels=training_labels, batch_size=150)
 
         # train network
         training_accuracy, training_loss, logits_output, _ = \
@@ -79,15 +73,15 @@ with tf.Session() as sess:
                                 label_placeholder: batch_training_labels})
 
 
-        # every 10 steps check accuracy
-        if step_count % 10 == 0:
-            batch_test_data, batch_test_labels = dataUtils.getBatch(data=test_data, labels=test_labels, batch_size=100)
+        # every 100 steps check accuracy
+        if step_count % 100 == 0:
+            batch_test_data, batch_test_labels = dataUtils.getBatch(data=test_data, labels=test_labels, batch_size=150)
             test_accuracy, test_loss, logits_output, _ = \
             sess.run([accuracy, loss, logits, train],
                      feed_dict={input_placeholder: batch_test_data,
                                 label_placeholder: batch_test_labels})
 
-
+            print("Logist {}". format(logits_output))
             print("Step Count:{}".format(step_count))
             print("Training accuracy: {} loss: {}".format(training_accuracy, training_loss))
             print("Test accuracy: {} loss: {}".format(test_accuracy, test_loss))
@@ -96,3 +90,4 @@ with tf.Session() as sess:
         # stop training after 1,000 steps
         if step_count > 1000:
             break
+
